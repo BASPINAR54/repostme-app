f08528136572ef14197236e8d5c9618722f36145
BASPINAR54 <contact@bsina.fr>
2025-12-23 16:33:28 +0100

    Updated index.tsx

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
 import { StyleSheet } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
   return (
-    <WebView
-      source={{ uri: 'https://repostme.com/login' }}
-      style={styles.webview}
-      javaScriptEnabled
-      domStorageEnabled
-      sharedCookiesEnabled
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        source={{ uri: 'https://repostme.com/login' }}
+        style={styles.webview}
+        javaScriptEnabled
+        domStorageEnabled
+        sharedCookiesEnabled
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   webview: {
     flex: 1,
   },
