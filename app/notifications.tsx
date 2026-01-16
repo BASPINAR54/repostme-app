edff9b569536778322a50425833676c762537bfa
BASPINAR54 <contact@bsina.fr>
2026-01-16 16:30:07 +0100

    Updated notifications.tsx

--- app/notifications.tsx
+++ app/notifications.tsx
@@ -206,7 +206,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 16,
-    paddingTop: 16,
+    paddingTop: 60,
     paddingBottom: 16,
     backgroundColor: '#ffffff',
     borderBottomWidth: 1,
